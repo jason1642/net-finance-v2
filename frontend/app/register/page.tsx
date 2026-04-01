5d0acdf0a9facc0b0289875b2d276fe44d0b631a
jason1642 <jason1642@gmail.com>
2026-04-01 19:51:43 -0400

    dev

--- frontend/app/register/page.tsx
+++ frontend/app/register/page.tsx
@@ -3,7 +3,7 @@ import { useEffect } from "react"
 import { useForm, SubmitHandler } from "react-hook-form"
 import { Description, Field, Input, Label } from '@headlessui/react'
 import Button from "@/components/button/Button"
-import registerValidations from './validations'
+// import registerValidations from './validations'
 
 
 interface IFormInput {
@@ -16,36 +16,66 @@ interface IFormInput {
 
 const formFields: { 
     name: string, 
-    id: keyof IFormInput
+    id: keyof IFormInput,
+    validations:{
+        required?: boolean,
+        maxLength?: number,
+        minLength?: number,
+        pattern?: RegExp,
+        validate?: (value: string, getValues: any) => boolean | string
+    }
 }[] = [
     {
         name: "First Name",
         id: "first_name",
+        validations:{
+            required: true,
+        maxLength: 20,
+        minLength: 2
+        }
     },
     {
         name: "Last Name",
         id: "last_name",
+        validations:{
+             required: true, 
+        maxLength: 20,
+        minLength: 2
+        }
     },
     {
         name: "Email",
         id: "email",
+        validations: {
+              required: true,
+        pattern: /^\S+@\S+$/i
+        }
     },
     {
         name: "Password",
         id: "password",
+        validations: {
+            required: true,
+        minLength: 8,
+        maxLength: 100,
+        }
     },
     {
         name: "Confirm Password",
         id: "confirm_password",
+        validations: {
+                required: true,
+        validate: (value, getValues) => value === getValues("password") || "Passwords do not match"
+        }
     }
 ]
 
 export default function Page() {
-    const { register, handleSubmit, formState:{ 
+    const { register, handleSubmit, getValues, watch, formState:{ 
         errors
     }} = useForm<IFormInput>()
     const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data);
-
+    // const passwordWatch = watch("password")
     // useEffect(()=>{
 
     //     console.log(getValues())
@@ -69,7 +99,7 @@ export default function Page() {
                 <Input 
                     className="mt-3 block w-full rounded-lg border-none bg-white/5 px-3 py-1.5 text-sm/6 text-white
             focus:not-data-focus:outline-none data-focus:outline-2 data-focus:-outline-offset-2 data-focus:outline-white/25"
-                 {...register(field.id, registerValidations[field.id])} />
+                 {...register(field.id, field.validations)} />
                  {errors[field.id] && <Description className="text-red-500">{errors[field.id]?.message}</Description>}
                 </Field>
                 ))}
